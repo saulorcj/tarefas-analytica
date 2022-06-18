131eef4b94289f18497a19af47f694c045d95f58
saulorcj <saulorcj@ic.ufrj.br>
2022-06-18 18:37:26 -0300

    Adicionando comentários.

--- P2/tarefa_4.py
+++ P2/tarefa_4.py
@@ -5,9 +5,12 @@ freq_by_numero = defaultdict(int)
 
 numero = input()
 while numero != "f":
+    # Verifica a formatação da entrada
     if re.search("^\d+$", numero):
         freq_by_numero[numero] += 1
     numero = input()
+
+# Itera o dicionário de forma ordenada
 for numero in sorted(freq_by_numero):
     frequencia = freq_by_numero[numero]
     print("O número {0} apareceu {1} {2}".format(numero,
