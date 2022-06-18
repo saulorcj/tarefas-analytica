9674c0a7559c4fce75e77be00801369c36c86724
saulorcj <saulorcj@ic.ufrj.br>
2022-06-17 21:24:56 -0300

    Concluindo a tarefa.

--- P2/tarefa_1.py
+++ P2/tarefa_1.py
@@ -2,9 +2,21 @@ import re
 
 while True:
     horario = input()
-    if re.search('^([0-1][0-9]|2[0-3]):[0-5][0-9]$', horario):
-        horas, minutos = map(int, horario.split(":"))
-    else:
+    if horario == "f":
+        break
+    if not re.search('^([0-1][0-9]|2[0-3]):[0-5][0-9]$', horario):
         print("Input inválido")
+        continue
+    horas, minutos = map(int, horario.split(":"))
 
-    break
+    # Formatando como ângulos
+    horas = (horas % 12) * 30
+    minutos *= 6
+
+    # ordena os ângulos
+    maior_ang, menor_ang = (horas, minutos) if horas > minutos else (minutos, horas)
+
+    # Verifica qual dos dois graus entre eles é o menor
+    grau_min = min(360 - maior_ang + menor_ang, maior_ang - menor_ang)
+
+    print("O menor ângulo é de {0}º".format(grau_min))
