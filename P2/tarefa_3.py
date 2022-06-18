0f44d8d248730b1a16843b95cd469705aaac928d
saulorcj <saulorcj@ic.ufrj.br>
2022-06-18 18:14:37 -0300

    Adicionando comentários.

--- P2/tarefa_3.py
+++ P2/tarefa_3.py
@@ -1,17 +1,20 @@
 import re
 
+# Listas com as notas e moedas disponíveis
 notas = [100, 50, 20, 10, 5, 2]
 moedas = [1, 0.5, 0.25, 0.1, 0.05, 0.01]
 
 while True:
     entrada = input()
 
+    # Interrompe o programa
     if entrada == "f":
         break
+
+    # Verifica a formatação da entrada
     if not re.search("^\d+.\d{2}$", entrada):
         print("Input Inválido")
         continue
-
     total = float(entrada)
 
     print("NOTAS:")
@@ -20,7 +23,7 @@ while True:
         total -= nota * qtd
         print("{0} nota(s) de R$ {1:.2f}".format(qtd, nota))
     print("\n"
-          "MOEDAS")
+          "MOEDAS:")
     for moeda in moedas:
         qtd = int(total // moeda)
         total -= moeda * qtd
