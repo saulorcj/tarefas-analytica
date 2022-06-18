2a92c1eaac04012643388f744a05ee2f10b3e8e3
saulorcj <saulorcj@ic.ufrj.br>
2022-06-18 17:43:08 -0300

    Alterando alguns detalhes.

--- P2/tarefa_1.py
+++ P2/tarefa_1.py
@@ -1,19 +1,19 @@
 import re
 
 while True:
-    horario = input()
-    if horario == "f":
-        break
-    if not re.search('^([0-1][0-9]|2[0-3]):[0-5][0-9]$', horario):
+    entrada = input()
+
+    # Verifica a formatação da entrada
+    if not re.search('^([0-1][0-9]|2[0-3]):[0-5][0-9]$', entrada):
         print("Input inválido")
         continue
-    horas, minutos = map(int, horario.split(":"))
+    horas, minutos = map(int, entrada.split(":"))
 
-    # Formatando como ângulos
+    # Converte para ângulos
     horas = (horas % 12) * 30
     minutos *= 6
 
-    # ordena os ângulos
+    # Ordena os ângulos
     maior_ang, menor_ang = (horas, minutos) if horas > minutos else (minutos, horas)
 
     # Verifica qual dos dois graus entre eles é o menor
