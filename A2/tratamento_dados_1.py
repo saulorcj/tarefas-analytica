2eb088671939ac494d30f4a461fd2726d9f5434b
saulorcj <saulorcj@ic.ufrj.br>
2022-06-19 23:40:26 -0300

    Simplificando um pouco o código.

--- A2/tratamento_dados_1.py
+++ A2/tratamento_dados_1.py
@@ -1,23 +1,25 @@
 import pandas as pd
-
 dados = pd.read_csv("../datasets/proporcao_pessoas_proximas_pnt.csv")
 
+
+def get_media_proporcao(ano, indicador):
+    """
+    :param ano: ano a ser selcionado
+    :param indicador: indicador a ser selecionado
+    :return: média da proporção de pessoas que moram perto de redes de transporte
+    """
+    media = dados[(dados["ano"] == ano) & (dados["indicador"] == indicador)]["prop_pessoas_proximas_pnt"].mean()
+    return media
+
+
 # Dados Gerais em 2019
-dados_geral = dados[(dados["ano"] == 2019) & (dados["indicador"] == "PNT Geral")]
-print(dados_geral.head())
-media_proporcao_geral = dados_geral["prop_pessoas_proximas_pnt"].mean()
+media_proporcao_geral = get_media_proporcao(2019, "PNT Geral")
 print("media de pessoas: {0:.2f}%".format(media_proporcao_geral))
-"""
+
 # Dados sobre mulheres negras em 2019
-dados_mulh_negras = dados[dados["ano"] == 2019][dados["indicador"] == "PNT Mulheres negras"]
-#print(dados_mulh_negras.head())
-media_proporcao_mulh_negras = dados_mulh_negras["prop_pessoas_proximas_pnt"].mean()
+media_proporcao_mulh_negras = get_media_proporcao(2019, "PNT Mulheres negras")
 print("media de mulheres negras: {0:.2f}%".format(media_proporcao_mulh_negras))
 
 # Dados sobre mulheres responsáveis por domicílios menos 2 salários mínimos
-dados_mulh_dom = dados[dados["ano"] == 2019][dados["indicador"] ==
-                                             "PNT Mulheres responsáveis por domicílio menos 2 salários mínimos"]
-print(dados_mulh_dom)
-media_proporcao_mulh_dom = dados_mulh_dom["prop_pessoas_proximas_pnt"].mean()
+media_proporcao_mulh_dom = get_media_proporcao(2019, "PNT Mulheres responsáveis por domicílio menos 2 salários mínimos")
 print("media de mulheres responsáveis por domicílios: {0:.2f}%".format(media_proporcao_mulh_dom))
-"""
