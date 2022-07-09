b9d78a5a6a5595fd8c5bba3413ebc1dd0b468a22
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-07-09 20:00:15 -0300

    pequenas alterações finais

--- P2/tarefa_4.py
+++ P2/tarefa_4.py
@@ -3,18 +3,18 @@ import re
 
 # Dicionário frequência por número
 # Se a chave não existir, ele a cria ligada a um inteiro com valor zero
-# Evita ter que criar um if só para saber se a chave existe
+# Evita ter que criar um if só para saber se a chave existe e realiza somente uma busca no HashMap
 freq_by_numero = defaultdict(int)
 
 numero = input()
 while numero != "f":
     # Verifica a formatação da entrada
     if re.search("^\d+$", numero):
-        freq_by_numero[numero] += 1
+        freq_by_numero[int(numero)] += 1
     numero = input()
 
 # Itera o dicionário de forma ordenada
-for numero in sorted(freq_by_numero):
+for numero in freq_by_numero:
     frequencia = freq_by_numero[numero]
     print("O número {0} apareceu {1} {2}".format(numero,
                                                  frequencia,
